95a27b7853d5a643466abe561ce5c6a0c815d476
shivam-bit <shivama5tronot@gmail.com>
2022-09-17 13:35:29 +0530

    fix: minor css and routing issues

--- src/components/core/Button.tsx
+++ src/components/core/Button.tsx
@@ -1,22 +1,18 @@
-
-const Button: any = ({triggerOnClick,children}) => {
-    const onClickHandler = (e) => {
-        triggerOnClick();
-        const buttonNode = e.currentTarget
-        buttonNode.classList.add('animate');
-        setTimeout(()=>{
-            buttonNode.classList.remove('animate')
-        },500);
-        // TODO: Add analytics event
-    }
+const Button: any = ({ triggerOnClick, classes, children }) => {
+  const onClickHandler = (e) => {
+    triggerOnClick();
+    const buttonNode = e.currentTarget;
+    buttonNode.classList.add('animate');
+    setTimeout(() => {
+      buttonNode.classList.remove('animate');
+    }, 500);
+    // TODO: Add analytics event
+  };
   return (
-        <button 
-        className="confetti-button"
-        onClick={onClickHandler}
-        >
-            {children}
-        </button>
-  )
-}
+    <button className={`confetti-button ${classes}`} onClick={onClickHandler}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
